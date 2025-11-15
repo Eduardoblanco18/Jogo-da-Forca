d402592839e7c8d6085e04a275bf696cccedcb2f
Eduardo Domingues Blanco <eduardodominguesblanco@gmail.com>
2025-11-14 22:10:58 -0300

    Adicionando chute e laço para o jogo

--- Jogo-da-Forca.cpp
+++ Jogo-da-Forca.cpp
@@ -5,4 +5,13 @@ using namespace std;
 int main () {
     string palavra_chave = "MELANCIA";
     cout << palavra_chave << endl;
+
+    bool nao_acertou = true, nao_enforcou = true;
+
+    while(nao_acertou && nao_enforcou){
+        char chute;
+        cin >> chute;
+
+        cout << "O seu chute foi " << chute << endl;
+    }
 }
