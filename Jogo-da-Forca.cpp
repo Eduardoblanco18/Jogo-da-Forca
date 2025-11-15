0f62c8df3e86428ef51d2169b75cdc12e5ee67cc
Eduardo Domingues Blanco <eduardodominguesblanco@gmail.com>
2025-11-14 23:20:27 -0300

    adicionando chutes errados

--- Jogo-da-Forca.cpp
+++ Jogo-da-Forca.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <vector>
 using namespace std;
-const string PALAVRA_SECRETA = "MELANCIA";
+const string PALAVRA_SECRETA = "GOIABADA";
 map<char, bool> chutou;
+vector<char> chutes_errados;
 
 bool letra_existe (char chute) {
     for(char letra : PALAVRA_SECRETA){
@@ -16,10 +18,20 @@ bool letra_existe (char chute) {
 }
 
 int main () {
+    cout << "*********************************" << endl;
+    cout << "***      Jogo da Forca        ***" << endl;
+    cout << "*********************************" << endl;
 
     bool nao_acertou = true, nao_enforcou = true;
 
     while(nao_acertou && nao_enforcou){
+
+        cout << "Chutes errados: ";
+        for(char letra : chutes_errados){
+            cout << letra << " "; 
+        }
+        cout << endl;
+
         for(char letra : PALAVRA_SECRETA){
             if(chutou[letra]){
                 cout << letra<< " ";
@@ -29,8 +41,8 @@ int main () {
         }
         cout << endl;
         
-        
         char chute;
+        cout << "Faça seu chute: ";
         cin >> chute;
 
         chutou[chute] = true;
@@ -39,6 +51,8 @@ int main () {
             cout << "Você acertou! Seu chute está na palavra!" << endl;
         } else {
             cout << "Você errou! Seu chute não está na palavra!" << endl;
+            chutes_errados.push_back(chute);
         }
+        cout << endl;
     }
 }
