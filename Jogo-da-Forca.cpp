6e35db346ade2a835ed5d988a59fbd8149b490b0
Eduardo Domingues Blanco <eduardodominguesblanco@gmail.com>
2025-11-14 22:41:05 -0300

    Verificação se a letra está na palavra secreta

--- Jogo-da-Forca.cpp
+++ Jogo-da-Forca.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+const string PALAVRA_SECRETA = "MELANCIA";
+
+bool letra_existe (char chute) {
+    for(char letra : PALAVRA_SECRETA){
+        if (chute == letra){
+            return true;
+        }
+    }
+
+    return false;
+}
 
 int main () {
-    string palavra_chave = "MELANCIA";
-    cout << palavra_chave << endl;
+    cout << PALAVRA_SECRETA << endl;
 
     bool nao_acertou = true, nao_enforcou = true;
 
@@ -12,6 +22,10 @@ int main () {
         char chute;
         cin >> chute;
 
-        cout << "O seu chute foi " << chute << endl;
+        if(letra_existe(chute)){
+            cout << "Você acertou! Seu chute está na palavra!" << endl;
+        } else {
+            cout << "Você errou! Seu chute não está na palavra!" << endl;
+        }
     }
 }
