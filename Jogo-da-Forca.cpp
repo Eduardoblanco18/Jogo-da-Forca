9aa5413e6380b5ae8c6509a1880477be0b71525e
Eduardo Domingues Blanco <eduardodominguesblanco@gmail.com>
2025-11-14 22:59:35 -0300

    Adicionando impressão da palavra secreta

--- Jogo-da-Forca.cpp
+++ Jogo-da-Forca.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 const string PALAVRA_SECRETA = "MELANCIA";
+map<char, bool> chutou;
 
 bool letra_existe (char chute) {
     for(char letra : PALAVRA_SECRETA){
@@ -14,13 +16,24 @@ bool letra_existe (char chute) {
 }
 
 int main () {
-    cout << PALAVRA_SECRETA << endl;
 
     bool nao_acertou = true, nao_enforcou = true;
 
     while(nao_acertou && nao_enforcou){
+        for(char letra : PALAVRA_SECRETA){
+            if(chutou[letra]){
+                cout << letra<< " ";
+            } else {
+                cout << "_ ";
+            }
+        }
+        cout << endl;
+        
+        
         char chute;
         cin >> chute;
+
+        chutou[chute] = true;
 
         if(letra_existe(chute)){
             cout << "Você acertou! Seu chute está na palavra!" << endl;
